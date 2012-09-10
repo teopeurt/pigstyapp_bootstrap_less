ce9df15c6cdba20e4f763704e28499a4f5c1518f
Don Onwunumah <teopeurt@gmail.com>
2012-09-10 23:08:12 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PigstyappBootstrapLess::Application.config.secret_token = '0fe5fbe1e599ba0c5736939808c72c775f32aa4569564060775fd553e361cafd6fbea7125593fb40037bfcd9e9c43300cee9da98f3df217c4eb58e506aa20db9'
